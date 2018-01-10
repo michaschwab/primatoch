d8cb2ab88714a50ce01f773bd9a4aa8c34dfe616
Koen <32582137+vanhaalen@users.noreply.github.com>
2018-01-10 23:05:13 +0100

    Added HTML and CSS for homepage.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,14 +5,15 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins'),
-hexrgba = require('postcss-hexrgba');
+hexrgba = require('postcss-hexrgba'),
+calc = require('postcss-calc');
 
 
 
 
 gulp.task('styles', function() {
     return gulp.src('app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, calc, nested, hexrgba, autoprefixer]))
         .on("error", function(errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end');
